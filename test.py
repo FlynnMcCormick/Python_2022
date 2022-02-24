46c3b9c7447e8714cd64ddad2c598f89ccabffbe
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-02-25 12:21:47 +1300

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,23 @@
 print("hello World")
 print("heyy")
 print(1+2*2)
+
+#Data type/ exe 4.1 /pg. 13
+
+print("Flynn")
+print("Mrs White")
+print("King's High")
+print("15")
+print("2047")
+print("115")
+print(658/7)
+print("A long time ago")
+print("candy crush")
+print("Digi Tec")
+
 """
+
+#Data type/ exe 4.4 /pg. 17 
+
+print("Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you")
+print("Starwars ep.1 \n ex squeeze me ")
