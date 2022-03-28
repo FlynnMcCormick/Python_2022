f3e1652ca2d8a87051d4c4cc614c6312a5e64d51
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-03-28 14:48:08 +1300

    Create game2.py

---
+++ game2.py
@@ -0,0 +1,34 @@
+from adventurelib import *
+
+
+#Imports
+#Define Rooms
+forrest_open("""
+ 
+	""")
+#Define Connections
+#Define Items
+#Define Bags
+#Add Items to Bags
+#Define any variables
+#Binds 
+#Main Function
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	start()
+
+
+
+
+if __name__ == '__main__':
+	main()
