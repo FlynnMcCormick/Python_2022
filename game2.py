3849e2d8369e27686f4d966d9851c90bb3bf1395
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-03-30 10:00:28 +1300

    ee

--- game2.py
+++ game2.py
@@ -3,9 +3,18 @@ from adventurelib import *
 
 #Imports
 #Define Rooms
-forrest_open("""
- 
+spawn_area("""
+	you are in a over grown field surounded by lush forests on all sides. there is a small log cabbin to the east aswell as a track to the north and south. there is a shimmer to the east.
 	""")
+
+log_cabbin("""
+	there is a small cooking space in the south east corner of the house and a large bed covered in animal hide in the south west corner of the room. you see a door leading out to the west and a door leading to the east.
+	""")
+
+behind_cabbin("""
+	there is a axe stabbed inside of a log and a pile of wood sitting aroud the outside of the house 
+	""")
+
 #Define Connections
 #Define Items
 #Define Bags
