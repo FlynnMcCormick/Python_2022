ea6f86c877efd4c9876b9416d2519952d6021e3e
FlynnMcCormick <Flymcc24@kingshigh.school.nz>
2022-04-27 15:55:16 +1200

    ee

--- game2.py
+++ game2.py
@@ -1,25 +1,24 @@
 from adventurelib import *
 Room.items = Bag()
-
 #Imports   #this is whee any imports are gonna be like music ect
 
 
 #Define Rooms    #this is where all toe room descriptions are 
 spawn_area = Room("""
-	you are in a over grown field surounded by lush forests on all sides. there is a small log cabbin to the east aswell as a track to the north and south. there is a shimmer to the east.
+	you are in a over grown field surounded by lush forests on all sides. there is a small log cabbin to the west as well as a hut to the east. there are exits to the north, east, south, and west.
 	""")
 
 log_cabbin = Room("""
-	there is a small cooking space in the south east corner of the house and a large bed covered in animal hide in the south west corner of the room. you see a door leading out to the west and a door leading to the east.
+	there is a small cooking space in the south east corner of the house and a large bed covered in animal hide in the south west corner of the room. there is a exit to the east.
 	""")
 
 
 tall_trees1 = Room("""
-	you are faced with tall trees streched across the path. the past leads west. 
+	you are faced with tall trees streched across the path. there is a small patch of herbs here. the past leads west. 
 	""")
 
 tall_trees2 = Room("""
-	you are faced with tall trees streched across the path. the past leads east and west. 
+	you are faced with tall trees streched across the path.there is a small patch of herbs here.  the past leads east and west. 
 	""")
 
 tall_trees3 = Room("""
@@ -35,7 +34,7 @@ tall_trees5 = Room("""
 	""")
 
 tall_trees6 = Room("""
-	you are faced with tall trees streched across the path. the past leads east and west. 
+	you are faced with tall trees streched across the path. there is a small patch of herbs here. the past leads east and west. 
 	""")
 
 elder_hut = Room("""
@@ -49,11 +48,13 @@ Dungeon = Room("""
 spawn_area.south = tall_trees4
 spawn_area.west = log_cabbin
 spawn_area.north = tall_trees3
+spawn_area.east = elder_hut
 tall_trees2.east = Dungeon
 tall_trees2.west = tall_trees1
 tall_trees3.north = tall_trees2
 tall_trees4.south = tall_trees5
 tall_trees5.west = tall_trees6
+
 
 #Define Items    #this is where i put all the descriptions for the items 
 Item.description = ""
@@ -76,25 +77,31 @@ tall_trees2.items.add(herb)
 
 tall_trees6.items.add(herb)
 
+
+
 #Define any variables    #this is wheree all the variables are kept
 current_room = spawn_area
 
 inventory = Bag()
 
+
 herb_count = 0
 
-dungeon_lock=True
+dungeon_lock = True 
 
 #Binds    #this is where the binds for the game are stored like the function to look or move
 @when ("go DIRECTION")
 def travel(direction):
 	global current_room
+	global dungeon_lock
 	if direction in current_room.exits():
 		current_room = current_room.exit(direction)
 		print(f"you go {direction}.")
 		print(current_room)
 		print(current_room.exits())
-
+	if current_room == Dungeon and inventory.find('key') == None:
+		print("you have died")
+		quit()
 
 @when('look')
 def look():
@@ -109,15 +116,17 @@ def look():
 @when('take ITEM')
 @when('pick up ITEM')
 def pickup(item):
-	#global dungeon_lock
+	global herb_count
 	if item in current_room.items:
 		t = current_room.items.take(item)
 		inventory.add(t)
 		print(f'you pick up the {item}')
-		if item=='herb':
-			herb_count+= 1
 	else:
 		print(f'you dont see a {item}')
+
+	if item =='herb':
+		herb_count += 1
+		print(f'you have {herb_count} herb')
 
 
 @when('inventory')
@@ -149,17 +158,16 @@ def use(item):
 		print("You can't use that here")
 
 
-
 #Main Function
 
 
 
-#def main():
-print(current_room)
-start()
+def main():
+	print(current_room)
+	start()
 
 
 
 
-#if __name__ == '__main__':
-	#main()
+if __name__ == '__main__':
+	main()
